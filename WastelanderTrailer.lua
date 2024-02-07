116ec4a8a8646b27d3859af500582c4d38114bc7
Tyler Chamberlain <hexarobi@gmail.com>
2024-02-07 10:00:44 -0600

    add adjust position menu

--- WastelanderTrailer.lua
+++ WastelanderTrailer.lua
@@ -1,7 +1,7 @@
 -- WastelanderTrailer
 -- by Hexarobi
 
-local SCRIPT_VERSION = "0.4"
+local SCRIPT_VERSION = "0.5"
 
 ---
 --- Auto-Updater Lib Install
@@ -37,6 +37,7 @@ if auto_updater == true then error("Invalid auto-updater lib. Please delete your
 local auto_update_config = {
     source_url = "https://raw.githubusercontent.com/hexarobi/stand-lua-wastelandertrailer/main/WastelanderTrailer.lua",
     script_relpath = SCRIPT_RELPATH,
+    draw_bounding_box = false,
 }
 auto_updater.run_auto_update(auto_update_config)
 
@@ -45,13 +46,81 @@ auto_updater.run_auto_update(auto_update_config)
 ---
 
 util.require_natives(1651208000)
+local quaternionLib = require("quaternionLib")
 
 ---
 --- State
 ---
 
-local state = {}
+local config = {
+    edit_offset_step = 1,
+    preview_bounding_box_color = {r=255,g=0,b=255,a=255}
+}
+
+local state = {
+    attached_vehicle = {
+        is_attached = false,
+        offset = { x = 0, y = 0, z = 0 }
+    }
+}
 local menus = {}
+
+
+---
+--- Bounding Box
+---
+
+local gizmo_minimum = memory.alloc()
+local gizmo_maximum = memory.alloc()
+local function get_entity_bounds(entity)
+    MISC.GET_MODEL_DIMENSIONS(ENTITY.GET_ENTITY_MODEL(entity), gizmo_minimum, gizmo_maximum)
+    local minimum_vec = v3.new(gizmo_minimum)
+    local maximum_vec = v3.new(gizmo_maximum)
+    local max_copy = v3.new(maximum_vec)
+    max_copy:sub(minimum_vec)
+    return {min = minimum_vec, max = maximum_vec, dimensions = max_copy}
+end
+
+local indices <const> = {{1, 2},{1, 4},{1, 8},{3, 4},{3, 2},{3, 5},{6, 5},{6, 8},{6, 2},{7, 4},{7, 5},{7, 8}}
+local function draw_bounding_box(entity, colour)
+
+    local rot = quaternionLib.from_entity(entity)
+    local pos = ENTITY.GET_ENTITY_COORDS(entity)
+
+    local bounds = get_entity_bounds(entity)
+
+    local minimum_vec = bounds.min
+    local maximum_vec = bounds.max
+
+    local vertices = {
+        v3.new(minimum_vec.x, maximum_vec.y, maximum_vec.z),    --local top_left
+        v3.new(minimum_vec.x, minimum_vec.y, maximum_vec.z),    --local bottom_left
+        v3.new(maximum_vec.x, minimum_vec.y, maximum_vec.z),    --local bottom_right
+        v3.new(maximum_vec),                                    --local top_right
+        v3.new(maximum_vec.x, minimum_vec.y, minimum_vec.z),    --local bottom_right_back
+        v3.new(minimum_vec),                                    --local bottom_left_back
+        v3.new(maximum_vec.x, maximum_vec.y, minimum_vec.z),    --local top_right_back
+        v3.new(minimum_vec.x, maximum_vec.y, minimum_vec.z)     --local top_left_back
+    }
+
+    for i = 1, #vertices, 1 do
+        local vert = vertices[i]
+        vert = rot:mul_v3(vert)
+        vert:add(pos)
+        vertices[i] = vert
+    end
+
+    for i = 1, #indices, 1 do
+        local vert_a = vertices[indices[i][1]]
+        local vert_b = vertices[indices[i][2]]
+
+        GRAPHICS.DRAW_LINE(
+                vert_a.x, vert_a.y, vert_a.z,
+                vert_b.x, vert_b.y, vert_b.z,
+                colour.r, colour.g, colour.b, colour.a
+        )
+    end
+end
 
 ---
 --- Attachments
@@ -68,10 +137,10 @@ local function update_attachment_position(attachment)
         attachment.collision = true
     end
     ENTITY.ATTACH_ENTITY_TO_ENTITY(
-        attachment.handle, attachment.root, attachment.bone_index or 0,
-        attachment.offset.x or 0, attachment.offset.y or 0, attachment.offset.z or 0,
-        attachment.rotation.x or 0, attachment.rotation.y or 0, attachment.rotation.z or 0,
-        false, true, attachment.collision, false, 2, true
+            attachment.handle, attachment.root, attachment.bone_index or 0,
+            attachment.offset.x or 0, attachment.offset.y or 0, attachment.offset.z or 0,
+            attachment.rotation.x or 0, attachment.rotation.y or 0, attachment.rotation.z or 0,
+            false, true, attachment.collision, false, 2, true
     )
 end
 
@@ -88,19 +157,29 @@ local function set_attachment_offset_for_root(attachment)
     local root_model = util.reverse_joaat(ENTITY.GET_ENTITY_MODEL(attachment.root))
     local dimensions = get_vehicle_dimension(attachment.handle)
 
+    if root_model == "flatbed" then
+        attachment.offset = {
+            x=0,
+            y=(dimensions.y / 2) - 2.73,
+            z=(dimensions.z / 2)
+        }
+        attachment.rotation = {x=0,y=0,z=0}
+    end
+
     if root_model == "wastelander" then
         attachment.offset = {
             x=0,
             y=(dimensions.y / 2) - 2,
-            z=(dimensions.min_vec.z) + 2.25
+            z=(dimensions.z / 2) + 0.8
         }
+        attachment.rotation = {x=0,y=0,z=0}
     end
 
     if root_model == "slamtruck" then
         attachment.offset = {
             x=0,
             y=(dimensions.y / 2) - 3,
-            z=(dimensions.min_vec.z) + 1.8
+            z=(dimensions.z / 2) + 0.2
         }
         attachment.rotation = {
             x=7,
@@ -112,6 +191,7 @@ local function set_attachment_offset_for_root(attachment)
 end
 
 local function attach(attachment)
+    attachment.is_attached = true
     attachment.position = ENTITY.GET_ENTITY_COORDS(attachment.root)
     ENTITY.SET_ENTITY_HAS_GRAVITY(attachment.handle, false)
     set_attachment_offset_for_root(attachment)
@@ -119,12 +199,17 @@ local function attach(attachment)
 
     ENTITY.SET_ENTITY_NO_COLLISION_ENTITY(attachment.root, attachment.handle)
 
+    menus.position_x.value = math.floor(attachment.offset.x * 100)
+    menus.position_y.value = math.floor(attachment.offset.y * -100)
+    menus.position_z.value = math.floor(attachment.offset.z * -100)
+
     return attachment
 end
 
 local function detach_attached_vehicle()
-    if state.attached_vehicle ~= nil then
+    if state.attached_vehicle ~= nil and state.attached_vehicle.is_attached then
         util.toast("Detaching "..state.attached_vehicle.name)
+        state.attached_vehicle.is_attached = false
         ENTITY.DETACH_ENTITY(state.attached_vehicle.handle, true, true)
         state.attached_vehicle = nil
     end
@@ -158,6 +243,18 @@ local function attach_nearest_vehicle()
 end
 
 ---
+--- Tick Handler
+---
+
+local function draw_bounding_box_tick()
+    if config.draw_bounding_box and state.attached_vehicle ~= nil then
+        draw_bounding_box(state.attached_vehicle.handle, config.preview_bounding_box_color)
+    end
+end
+
+util.create_tick_handler(draw_bounding_box_tick)
+
+---
 --- Menus
 ---
 
@@ -168,6 +265,9 @@ end)
 menu.action(menus.spawn_truck, "Slamtruck", {}, "Spawn a Slamtruck for towing", function()
     menu.trigger_commands("slamtruck")
 end)
+menu.action(menus.spawn_truck, "Flatbed", {}, "Spawn a Slamtruck for towing", function()
+    menu.trigger_commands("flatbed")
+end)
 
 menu.action(menu.my_root(), "Attach", {}, "Any close proximity vehicles will be attached to your current one", function()
     attach_nearest_vehicle()
@@ -175,4 +275,20 @@ end)
 
 menu.action(menu.my_root(), "Detach", {}, "", function()
     detach_attached_vehicle()
+end)
+
+
+menus.adjust_position = menu.list(menu.my_root(), "Adjust Position")
+
+menus.position_x = menu.slider_float(menus.adjust_position, "X: Left / Right", { "wastetrailerposx"}, "", -10000000, 10000000, math.floor(state.attached_vehicle.offset.x * 100), config.edit_offset_step, function(value)
+    state.attached_vehicle.offset.x = value / 100
+    update_attachment_position(state.attached_vehicle)
+end)
+menus.position_y = menu.slider_float(menus.adjust_position, "Y: Forward / Back", {"wastetrailerposy"}, "", -10000000, 10000000, math.floor(state.attached_vehicle.offset.y * -100), config.edit_offset_step, function(value)
+    state.attached_vehicle.offset.y = value / -100
+    update_attachment_position(state.attached_vehicle)
+end)
+menus.position_z = menu.slider_float(menus.adjust_position, "Z: Up / Down", {"wastetrailerposz"}, "", -10000000, 10000000, math.floor(state.attached_vehicle.offset.z * -100), config.edit_offset_step, function(value)
+    state.attached_vehicle.offset.z = value / -100
+    update_attachment_position(state.attached_vehicle)
 end)
